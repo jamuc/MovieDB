26974df9f01f6f3e961e31517595c5b1af3aefe6
Jason Franklin <jason.b.frankllin@gmail.com>
2020-12-12 10:21:51 +0100

    Add MovieKit framework to project

---
+++ MovieKit/MovieKit.h
@@ -0,0 +1,18 @@
+//
+//  MovieKit.h
+//  MovieKit
+//
+//  Created by Jason Franklin on 12.12.20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MovieKit.
+FOUNDATION_EXPORT double MovieKitVersionNumber;
+
+//! Project version string for MovieKit.
+FOUNDATION_EXPORT const unsigned char MovieKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MovieKit/PublicHeader.h>
+
+
