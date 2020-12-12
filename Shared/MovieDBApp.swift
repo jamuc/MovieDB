d6698ce87d8d1083858f3a3fad96bc91409502d7
Jason Franklin <jason.b.frankllin@gmail.com>
2020-12-12 10:10:12 +0100

    Initial Commit

---
+++ Shared/MovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBApp.swift
+//  Shared
+//
+//  Created by Jason Franklin on 12.12.20.
+//
+
+import SwiftUI
+
+@main
+struct MovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
